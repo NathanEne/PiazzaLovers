51c60358ef65f1bdace92fd05a24662d5dc7ee66
benjaminmesser <47367888+benjaminmesser@users.noreply.github.com>
2019-02-16 15:41:26 -0700

    Update HelloWorld.java

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package HelloWorld;
 
 public class HelloWorld {
         public static void main(String[] args){
-                System.out.println("henlo");
+                System.out.println("We are not gonna get first.");
 
         }
 }
