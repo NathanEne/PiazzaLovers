1a63599f863034e25051b34200b1d385a49c05cf
Harry <harry.chen1@ucalgary.ca>
2019-02-16 17:13:25 -0700

    window added

---
+++ HelloWorld/src/Window.java
@@ -0,0 +1,32 @@
+package HelloWorld.src;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.*;
+
+public class BankApp extends Application {
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public void start(Stage mainStage){
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+    }
+
