0181d5d275e2b3d32a4f3071215057a9f077be58
Harry <harry.chen1@ucalgary.ca>
2019-02-16 20:57:19 -0700

    basic canvas

--- HelloWorld/src/Window.java
+++ HelloWorld/src/Window.java
@@ -3,19 +3,51 @@ package HelloWorld.src;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+
+import java.awt.*;
 
 public class Window extends Application {
 
     public static void main(String[] args) throws Exception {
         launch(args);
+
     }
 
     @Override
     public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+
+        primaryStage.setResizable(false);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+
+        Scene scene = new Scene(root, bounds.getWidth(), bounds.getMinY());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("It' Snow Problem");
+
+        Canvas canvas = new Canvas(1000, 1000);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        root.getChildren().add(canvas);
+
+        gc.strokeOval(0,0,200,200);
+
+        primaryStage.show();
+    }
+
+            /*
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -26,13 +58,6 @@ public class Window extends Application {
             }
         });
 
-        StackPane root = new StackPane();
         root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+        */
 }
